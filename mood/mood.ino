7996eacec03ee22b35b3619c59e56cd2621354ec
SkeldrBoii <mateo.lundqvist@gmail.com>
2021-10-05 10:32:33 +0200

    finished code

--- mood/mood.ino
+++ mood/mood.ino
@@ -15,5 +15,9 @@ void loop() {
     potVal = analogRead(potPin);
     Serial.print("potVal: ")
     Serial.print(potVal)
-    angle = map(potVal, 0, 1023)
+    angle = map(potVal, 0, 1023, 0, 179);
+    Serial.print(angle)
+
+    myServo.write(angle);
+    delay(15);
 }
